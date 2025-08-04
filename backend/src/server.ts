2240a5b559cc810ac57f0648c25cc13ebdd69ecb
Khoa Cao <kqcao@wisc.edu>
2025-08-04 17:15:59 -0500

    feat: :sparkles: Create RDS on AWS. Connect from local machine. Using prsima for ORM
    Reminder: check public accessibility +  VPC policy groups when cannot connect to database

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,18 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
 import { errorHandler } from "./middleware/errorHandler";
+import cors from 'cors'
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+
 app.use(express.json());
+app.use(cors())
 app.use(errorHandler)
 
-app.get('/', (_req, res) => {
-  res.send('Backend listening on port ' + port)
-})
+app.get('/api/health', (req, res) => {
+  res.status(200).json({ status: 'ok', message: 'Backend is running!' });
+});
 
 app.use((_req, res) => {
   res.status(404).json({ error: 'Not found' })
