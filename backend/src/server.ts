41b591b28a334f58688849a558b3b0ed28cc13be
Khoa Cao <kqcao@wisc.edu>
2025-08-13 16:48:58 -0500

    feat: :sparkles: [BACKEND] implement playwright/nunjucks for pdf generation

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,16 +3,96 @@ import dotenv from "dotenv";
 import { errorHandler } from "./middleware/errorHandler";
 import cors from 'cors';
 import patientRouter from './routes/patient';
+import nunjucks from "nunjucks";
+import { chromium, Browser } from "@playwright/test";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+const baseUrl = process.env.PUBLIC_BASE_URL || `http://127.0.0.1:${port}`;
 
 
 app.use(express.json());
 app.use(cors())
 app.use(errorHandler)
+app.use(express.static('public'))
+
+nunjucks.configure("views", { autoescape: true });
+
+// Keep a browser alive for speed
+let browserPromise: Promise<Browser> | null = null;
+async function getBrowser() {
+  if (!browserPromise) browserPromise = chromium.launch({ args: ["--no-sandbox"] });
+  return browserPromise;
+}
+
+const MOCK_DATA = {
+  "name": "Trần Văn Huy",
+  "YOB": "2008",
+  "gender": "Male",
+  "address": "13 Đào Duy Từ, P5, Q10, TP.HCM",
+  "MP": {
+    "UCVA": "6/18",
+    "SPH": "-2.00",
+    "CYL": "-0.50",
+    "AX": "180",
+    "BCVA": "6/6",
+    "ADD": "+0.00"
+  },
+  "MT": {
+    "UCVA": "6/12",
+    "SPH": "-1.75",
+    "CYL": "-0.50",
+    "AX": "170",
+    "BCVA": "6/6",
+    "ADD": "+0.00"
+  },
+  "current_glasses": "Single vision, polycarbonate, AR coating",
+  "right_eye": "SPH -2.00 CYL -0.50 AX 180",
+  "left_eye": "SPH -1.75 CYL -0.50 AX 170",
+  "reassessmentTime": "3"
+}
+
+app.get("/test-pdf", async (req, res, next) => {
+  try {
+    const {
+      name, YOB, gender, address, MT, MP,
+      current_glasses, right_eye, left_eye, reassessmentTime
+    } = MOCK_DATA;
+
+
+    const html = nunjucks.render("record.njk", {
+      baseUrl,name, YOB, gender, address, MT, MP,
+      current_glasses, right_eye, left_eye, reassessmentTime,
+      phone_number: "0913963003",
+      email: "drkhanhtrang.ophth@gmail.com"
+    });
+
+    const browser = await getBrowser();
+    const page = await browser.newPage();
+    await page.setContent(html, { waitUntil: "networkidle" });
+
+    const pdf = await page.pdf({
+      printBackground: true,
+      margin: { top: "0", right: "0", bottom: "0", left: "0" },
+      preferCSSPageSize: true
+    });
+    await page.close();
+
+    const filename = "test.pdf";
+
+    res
+      .set({
+        "Content-Type": "application/pdf",
+        "Content-Disposition": `attachment; filename="${filename}"`
+      })
+      .send(pdf);
+  } catch (err) {
+    next(err);
+  }
+});
+
 
 app.use("/api/patient", patientRouter);
 
